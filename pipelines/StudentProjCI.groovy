219e727239ea973eb9b92f9ed8ddd1d3737457fd
Raghu K <raghuk.vit@gmail.com>
2018-09-22 01:35:02 +0000

    Updating jenkins-jobs

--- pipelines/StudentProjCI.groovy
+++ pipelines/StudentProjCI.groovy
@@ -2,5 +2,26 @@ node {
     stage('Clone-Repo') {
         echo 'Cloning Repository'
     }
-
+    stage('Build'){
+        echo 'Doing Compile'
+    }
+    stage('Code-Quality') {
+        echo 'Running Sonarqube scans'
+    }
+    stage('Packaging') {
+        echo 'Making War ffile'
+    }
+    stage('Dev Deploy') {
+        echo "Deploy the war file into Dev Server"
+    }
+    stage('Selenium Testing') {
+        echo "Selenium Testing"
+    }
+    stage('API Testing'){
+        echo "API Testing"
+    }
+    stage('Upload Artifacts') {
+        echo "Upload code to artifacts"
+    }
+ 
 } 
