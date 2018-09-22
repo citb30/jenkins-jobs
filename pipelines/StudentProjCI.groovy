52f17cab0aec350b8c131552af5102edf5dee1ff
Raghu K <raghuk.vit@gmail.com>
2018-09-22 02:17:27 +0000

    Updating jenkins-jobs

--- pipelines/StudentProjCI.groovy
+++ pipelines/StudentProjCI.groovy
@@ -1,18 +1,27 @@
 node {
     stage('Clone-Repo') {
-        git 'https://github.com/citb30/studentapp-code.git'
+        dir('CODE') {
+            git 'https://github.com/citb30/studentapp-code.git'
+        }
     }
     stage('Build'){
-        sh "mvn compile"
+        sh "cd CODE ; mvn compile"
     }
     stage('Code-Quality') {
-        sh "mvn sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=9fd9a621cffbfb18e3fa64e4455d151250a2deb3"
+        sh "cd CODE ; mvn sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -Dsonar.login=9fd9a621cffbfb18e3fa64e4455d151250a2deb3"
     }
     stage('Packaging') {
-        sh 'mvn package'
+        sh 'cd CODE; mvn package'
     }
     stage('Dev Deploy') {
         build job: 'INSTANCE-CREATE', parameters: [string(name: 'PROJECT_NAME', value: 'student'), string(name: 'ENVIRONMENT', value: 'dev'), string(name: 'SERVER_NAME', value: 'studevapp01'), booleanParam(name: 'RECREATE', value: true)]
+        dir('ANSIBLE') {
+            git credentialsId: 'gitrouser', url: 'http://104.196.127.109/engineers/ansible.git'
+        }
+        sh '''
+        cd ANSIBLE
+echo 'studevapp01' >hosts 
+'''
     }
     stage('Selenium Testing') {
         echo "Selenium Testing"
