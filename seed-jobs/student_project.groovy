b16bcff61780cb3f539a2ef989f6f3d96f6b095d
Raghu K <raghuk.vit@gmail.com>
2018-09-28 01:24:27 +0000

    Updating jenkins-jobs

--- seed-jobs/student_project.groovy
+++ seed-jobs/student_project.groovy
@@ -1,4 +1,7 @@
 pipelineJob('StudentProj-CI') {
+    parameters {
+        stringParam('RELEASE_VERSION', 'NA', 'Enter Release Version')
+    }
     definition {
         cps {
             script(readFileFromWorkspace('pipelines/StudentProjCI.groovy'))
